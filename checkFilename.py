76c664f5f0f91176185a8b59c4aa3748da8c8e90
tmoleski_linux <tmoleski_linux@U-1AVED6MUX3MER.driveohio-dot.com>
2024-02-21 11:12:16 -0500

    added copy and updated of metafiles

--- checkFilename.py
+++ checkFilename.py
@@ -1,6 +1,8 @@
 import os
 import re
-
+import glob
+import ast, json
+import shutil
 def check_consistency(base_directory):
     directory_prefixes = {}  # Dictionary to store prefixes for each directory
     
@@ -30,19 +32,50 @@ def check_consistency(base_directory):
             
             # Store the set of prefixes for this directory
             directory_prefixes[subdirectory] = prefixes
-            
-            print()  # Add an empty line for better readability
     
     # Check for consistency and print the result
     print("Consistency check result:")
     for directory, prefixes in directory_prefixes.items():
         if len(prefixes) > 1:
             print(f"Directory {directory} has multiple prefixes: {prefixes}")
-        else:
-            print(f"Directory {directory} has consistent prefixes: {prefixes}")
+
+            filelist = sorted(glob.glob(base_directory+"/"+directory))
+
+            data_dir = base_directory+directory
+
+            print(data_dir)
+
+            for fname in filelist:
+                morefiles = sorted(glob.glob(fname+"/*"))
+                for mfname in morefiles:
+                    if(mfname.endswith(".json") and 'VanDrive_Comments' not in mfname and 'comments' not in mfname):
+                        print(mfname)
+                        
+                        # for pre in prefixes:
+                        #     # Copy the JSON file
+                        #     try:
+                        #         shutil.copyfile(mfname, data_dir+"/"+str(pre)+".json")
+
+                        #         # Read the copied JSON file
+                        #         with open(data_dir+"/"+str(pre)+".json", 'r') as file:
+                        #             data = json.load(file)
+
+                        #         data['file']['filebase'] = str(pre) + '.' + 'record' + '.'
+
+                        #         # Write %the updated data back to the file
+                        #         with open(data_dir+"/"+str(pre)+".json", 'w') as file:
+                        #             print("DUMPED:", data_dir+"/"+str(pre)+".json")
+                        #             json.dump(data, file, indent=4)
+                        #     except:
+                        #         print("PROBLEM WITH", mfname)
+                        #         continue
+
+
+        # else:
+        #     print(f"Directory {directory} has consistent prefixes: {prefixes}")
 
 # Provide the path to the desired directory
-base_directory = "/home/croback_linux/s3bucket/Deployment_2_SEOhio/Blue Route/TRCVan2/"
+base_directory = "/home/tmoleski_linux/s3bucket/Deployment_2_SEOhio/GreenRoute/OU Pacifica/"
 
 # Call the function to check consistency
 check_consistency(base_directory)
