c1fd0c52aa04519c45310f1b36d50110e14d8a95
camroback <cr721017@ohio.edu>
2024-02-21 15:09:24 -0500

    Create scripts to pull cyber info

---
+++ cameronExp.py
@@ -0,0 +1,251 @@
+#!/bin/python3
+
+# import packages
+import sys, time, os
+from importlib import import_module
+import numpy as np
+import cv2
+from PIL import Image
+import io
+import google.protobuf as protobuf
+import google.protobuf.descriptor_pb2 as descriptor_pb2
+import apollopy.proto.record_pb2 as record_pb2
+import apollopy.proto.proto_desc_pb2 as proto_desc_pb2
+from google.protobuf.json_format import MessageToJson
+import json
+from tqdm import tqdm
+from cybertools.cyberreaderlib import ProtobufFactory, RecordReader, RecordMessage
+import base64
+from datetime import datetime
+import utm
+
+###########################################################
+class VideoExporter:
+    
+    def __init__(self, camera_topic, export_dir):
+        # Topics
+        self.camera_topic = camera_topic
+        self.export_folder = ""
+        self.export_dimensions = (1920, 1080)
+        self.image = None
+        self.addMeta = True
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.videWriter = cv2.VideoWriter(export_dir + "ExportedInfo.avi", self.fourcc, 20.0, self.export_dimensions)
+
+    def stringToImage(self):
+        self.image = base64.b64decode(self.image)
+        self.image = Image.open(io.BytesIO(self.image))        
+        self.image_ts = 0
+        return self.image_ts
+
+    def toRGB(self):
+        self.image = cv2.cvtColor(np.array(self.image), cv2.COLOR_BGR2RGB)
+
+    def add_frame(self):
+        img = cv2.resize(self.image, self.export_dimensions)
+        self.videWriter.write(img)
+
+    def export_video(self):
+        self.videWriter.release()
+        print('')
+        print('VIDEO RELEASED') 
+        print('')
+
+###########################################################
+
+class GetMetadataToJson():
+    def __init__(self, export_dir):
+        print(export_dir)
+        self.filename = export_dir + "ExportedInfo.json"
+        print(self.filename)
+        self.frames = []
+
+    def createHeader(self, header_json):
+        self.frames = [header_json] + self.frames
+        pass
+
+    def export(self):
+        with open(self.filename, 'a') as json_file:
+            json.dump(self.frames, json_file, indent=4)
+            json_file.write('\n')
+
+###########################################################
+
+def initReader(filename):
+    unqiue_channel = []
+    pbfactory = ProtobufFactory()
+    reader = RecordReader(filename)
+    
+    for channel in reader.GetChannelList():
+        desc = reader.GetProtoDesc(channel)
+        pbfactory.RegisterMessage(desc)
+        unqiue_channel.append(channel)
+        
+    message = RecordMessage()
+    return message, reader, pbfactory
+
+###########################################################
+
+if __name__ == "__main__":
+    ### OPTIONS ###
+    # file_set = 1698251665
+    direct = "/home/croback/CyberProcessor/Cyber/"
+
+    export_dir = "/home/croback/CyberProcessor/CyberExport/"
+
+    print(export_dir)
+
+    if not os.path.exists(export_dir):
+        # Create the directory if it doesn't exist
+        os.makedirs(export_dir)
+        print(f"Directory '{export_dir}' created successfully.")
+    else:
+        print(f"Directory '{export_dir}' already exists.")
+
+    # TOPICS
+    image_handler = VideoExporter(camera_topic="/apollo/sensor/camera/front_6mm/image/compressed", export_dir=export_dir)
+    localization_topic = "/apollo/localization/pose"
+    chassis_topic = "/apollo/canbus/chassis"
+    
+    # FILE LOCATION
+    # direct = "/mnt/h/cyber_bags/1698251665/"
+    # direct = "/media/travis/moleski1/cyber_bags/1698251665"
+    # direct = "/media/autobuntu/chonk/chonk/git_repos/apollo/10252023_blue_route/"
+
+    # VIDEO
+    showVid = False
+    
+    # FILE CUTOFF
+    max_files_to_process = 1
+    early_break = False
+    
+    ### VAR INIT ###
+
+    # IMPORT FILE CHECK
+    if direct.endswith("/"):
+        pass
+    else:
+        direct = direct + "/"
+        
+    files = sorted(os.listdir(direct))
+
+    
+    # LIST FILES IN ORDER
+    
+    
+    # JSON INIT
+    json_export = GetMetadataToJson(export_dir)
+    
+    # COUNTERS
+    file_count = 0
+    frame_count = 0
+    
+    # ARRAYS
+    loc_data_to_metadata = {}
+    
+
+    ### MAIN CODE ###
+    
+    for file in files:
+
+        # Get the file
+        filename = os.path.join(direct,file)
+        print(filename)
+
+        
+        if filename.endswith('.json'):
+            print("FOUND METADATA IN: ", filename)
+            j_file = open(filename)
+            metadata = json.load(j_file)
+            json_export.createHeader(metadata)
+            continue
+        
+        else:
+
+            message, reader, pbfactory = initReader(filename)
+            
+            # print(message)
+            # print(type(message))
+            
+            # Get meta data into arrays
+            while reader.ReadMessage(message):
+                
+                message_type = reader.GetMessageType(message.channel_name)
+                msg = pbfactory.GenerateMessageByType(message_type)
+                msg.ParseFromString(message.content)
+                
+                if(message.channel_name == localization_topic): 
+    
+                    locdata = MessageToJson(msg)
+                    locdata = json.loads(locdata)
+
+                    
+                if(message.channel_name == chassis_topic):
+                    
+                    chasdata = MessageToJson(msg)
+                    chasdata = json.loads(chasdata)
+                    
+                if(message.channel_name == image_handler.camera_topic):
+
+                    
+                    imgdata = MessageToJson(msg)
+                    imgdata = json.loads(imgdata)
+                    
+                    # Get the timestamp of the frame
+                    img_ts = (imgdata['header']['timestampSec'])
+
+                    try:
+
+                        # Append data to a frame
+                        frame = {
+                            'localization': locdata,
+                            'chassis': chasdata
+                        }
+                        # Append to the json variable
+                        json_export.frames.append(frame)
+                        
+                        # print(json_export.getMatchedLocalizationMetaData(img_ts, localization_data.localization_data))
+                        # print(loc_data_to_metadata[frame_count])
+
+                        image_handler.image = imgdata['data']
+                        image_ts = image_handler.stringToImage()
+                        image_handler.toRGB()
+                        image_handler.add_frame()
+
+                        if showVid:
+
+                            lat,lon = utm.to_latlon(locdata['pose']['position']['x'], locdata['pose']['position']['y'], 17, 'S')
+
+                            lat = round(lat,5)
+                            lon = round(lon,5)
+
+                            driving_mode = chasdata['drivingMode']
+                            speed = round(chasdata['speedMps'] * 2.23694, 2)
+
+                            text = f'Frame: {frame_count}, Lat: {lat}, Lon: {lon}, Driving Mode: {driving_mode}, Speed (mph): {speed}'
+                            cv2.putText(image_handler.image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                            cv2.imshow(message.channel_name, image_handler.image)
+                            cv2.waitKey(30)
+                            
+                        frame_count += 1
+
+                    except:
+                        continue
+        
+
+
+            # Break Condition
+            file_count += 1
+            
+            if early_break:
+                
+                if file_count == max_files_to_process:
+                    
+                    break
+            
+    image_handler.export_video()
+    json_export.export()
+    
+    
+
+            # print("Message Count %d" % count)
---
+++ cameronShowExpVid.py
@@ -0,0 +1,120 @@
+import cv2
+import json
+import utm
+import math
+
+def read_metadata_from_json(json_file):
+    with open(json_file, 'r') as file:
+        metadata = json.load(file)
+    return metadata
+
+def main(video_path, json_file):
+    # Read metadata from JSON file
+    metadata = read_metadata_from_json(json_file)
+
+    # Open video file
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Error: Couldn't open video file.")
+        return
+
+    # Get video properties
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can also use 'XVID' or other codecs depending on your preference
+
+    # Initialize VideoWriter object
+    out = cv2.VideoWriter(output_video_file, fourcc, fps, (frame_width, frame_height))
+
+    # Get video properties
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Loop through frames
+    for frame_number in range(frame_count):
+
+        ret, frame = cap.read() 
+
+        if not ret:
+            print("Error reading frame.")
+            break
+
+        # Get metadata for the current frame
+        frame_metadata = metadata[frame_number+2]
+
+        # Extract values from metadata
+        chassis = frame_metadata.get('chassis')
+        loco = frame_metadata.get('localization')
+
+        van_time = f'{round(loco["header"]["timestampSec"], 3):.3f}'
+
+        lat, lon = utm.to_latlon(loco['pose']['position']['x'], loco['pose']['position']['y'], 17, 'S')
+        lat = f'{lat:.3f}'
+        lon = f'{lon:.3f}'
+        alt = f'{loco["pose"]["position"]["z"]:.3f}'
+
+        driving_mode = chassis['drivingMode']
+        speed = f'{chassis["speedMps"] * 2.23694:.2f}'
+        
+        heading = f'{loco["pose"]["heading"] * (180/math.pi):.3f}'
+
+        # Display values on the screen
+        text_line1 = 'OHIO UNIVERSITY IDEAS LAB'
+        text_line2 = f'Frame: {frame_number},Timestamp: {van_time}, Lat: {lat}, Lon: {lon}, Alt (m): {alt}'    # NEED TO CHECK THE UNITS FOR HEADING AND ALTITUDE
+        text_line3 = f'Speed (mph): {speed}, Heading(deg): {heading}, Driving Mode: {driving_mode}'
+
+        # Set the position for the first line above the existing two lines
+        position_line1 = (10, 30)
+
+        # Increase the font size
+        font_scale = 1.25  # Adjust the scale as needed
+
+        cv2.putText(frame, text_line1, position_line1, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 0, 0), 7)  # Black outline
+        cv2.putText(frame, text_line1, position_line1, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 255, 255), 2)  # Yellow text
+
+        # Calculate the height of the text to adjust the position for the second line
+        text_size_line1 = cv2.getTextSize(text_line1, cv2.FONT_HERSHEY_COMPLEX, font_scale, 7)[0]
+        text_height_line1 = text_size_line1[1]
+
+        # Set the position for the second line below the first line
+        position_line2 = (10, position_line1[1] + text_height_line1 + 10)
+
+        # Increase the font size for the second line
+        cv2.putText(frame, text_line2, position_line2, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 0, 0), 7)  # Black outline
+        cv2.putText(frame, text_line2, position_line2, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 255, 255), 2)  # Yellow text
+
+        # Calculate the height of the text to adjust the position for the third line
+        text_size_line2 = cv2.getTextSize(text_line2, cv2.FONT_HERSHEY_COMPLEX, font_scale, 7)[0]
+        text_height_line2 = text_size_line2[1]
+
+        # Set the position for the third line below the second line
+        position_line3 = (10, position_line2[1] + text_height_line2 + 10)
+
+        # Increase the font size for the third line
+        cv2.putText(frame, text_line3, position_line3, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 0, 0), 7)  # Black outline
+        cv2.putText(frame, text_line3, position_line3, cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 255, 255), 2)  # Yellow text
+
+        out.write(frame)
+        # Show the frame
+        cv2.imshow('Video with Metadata', frame)
+
+        # Break the loop if the user presses 'q'
+        if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
+            break
+
+    # Release video capture object and close window
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    # Specify the path to the video file and the JSON file
+    video_path = "./CyberExport/ExportedInfo.avi"
+    met_file =   "./CyberExport/ExportedInfo.json"
+    output_video_file = "./OutputVid.mp4"
+
+      # Run the main function
+    main(video_path, met_file)
+
