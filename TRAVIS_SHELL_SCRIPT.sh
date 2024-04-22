04c1a148a60cdffea8d906db3f0634ca1d6a8f29
camroback <cr721017@ohio.edu>
2024-04-22 16:20:38 -0400

    add shell script

---
+++ TRAVIS_SHELL_SCRIPT.sh
@@ -0,0 +1,38 @@
+# Fileset 1
+python TESTgetMetadataExp.py "1691091468" "20230803153748"
+
+# Fileset 2
+python TESTgetMetadataExp.py "1691091730" "20230803154210"
+
+# Fileset 3
+python TESTgetMetadataExp.py "1694718634" "20230914151034"
+
+# Fileset 4
+python TESTgetMetadataExp.py "1695324094" "20230921152134"
+
+# Fileset 5
+python TESTgetMetadataExp.py "1695927575" "20230928145935"
+
+# Fileset 6
+python TESTgetMetadataExp.py "1696532161" "20231005145601"
+
+# Fileset 7
+python TESTgetMetadataExp.py "1696533326" "20231005151526"
+
+# Fileset 8
+python TESTgetMetadataExp.py "1697137664" "20231012150744"
+
+# Fileset 9
+python TESTgetMetadataExp.py "1697138744" "20231012152544"
+
+# Fileset 10
+python TESTgetMetadataExp.py "1697742077" "20231019150117"
+
+# Fileset 11
+python TESTgetMetadataExp.py "1697743021" "20231019151701"
+
+# Fileset 12
+python TESTgetMetadataExp.py "1698347206" "20231026150646"
+
+# Fileset 13
+python TESTgetMetadataExp.py "1698348088" "20231026152128"
